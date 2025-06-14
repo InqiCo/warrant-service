f874a3957be1aad5c796cce71364868e97b49b84
samueloliveiraf <samueldiablo73@gmail.com>
2025-06-13 22:05:53 -0300

    fix: adjusment to code

--- app/config.py
+++ app/config.py
@@ -8,5 +8,7 @@ load_dotenv()
 class Settings:
     URL_RABBIT_MQ: str = getenv('URL_RABBIT_MQ')
     RECAPTCHA_KEY: str = getenv('RECAPTCHA_KEY')
+    MONGO_URL: str = getenv('MONGO_URL')
+    DB_NAME: str = getenv('DB_NAME')
 
 settings = Settings()
