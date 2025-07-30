b1e4a0beae7a3f417d4b1dc6bbae73244db2afe4
samueloliveiraf <samueldiablo73@gmail.com>
2025-07-30 20:07:28 -0300

    add chanel coment to code

--- main.py
+++ main.py
@@ -16,13 +16,17 @@ from service.save_result import save_result
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     connection = await connect_robust(settings.URL_RABBIT_MQ)
-    channel = await connection.channel()
-
-    queue = await channel.declare_queue(f'publish-warrants-{settings.ENVIRONMENT}', durable=True)
 
     max_retries = 5
+    consumers = []
 
-    async def consume(queue, name_queue):
+    async def consume(channel_id: int):
+        channel = await connection.channel()
+        await channel.set_qos(prefetch_count=1)
+
+        queue_name = f'publish-warrants-{settings.ENVIRONMENT}'
+        queue = await channel.declare_queue(queue_name, durable=True)
+
         async with queue.iterator() as queue_iter:
             async for message in queue_iter:
                 async with message.process(ignore_processed=True):
@@ -33,13 +37,14 @@ async def lifespan(app: FastAPI):
                         retry_count = body.get('retry_count', 0)
 
                         logger.info(
-                            f'[{name_queue}] Mensagem recebida: {body["query_id"]} (tentativa {retry_count + 1})')
+                            f'[consumer-{channel_id}] Mensagem recebida: {body["query_id"]} (tentativa {retry_count + 1})'
+                        )
 
                         await collection_queries.update_one(
                             {'_id': body['query_id']},
                             {
                                 '$set': {
-                                    f'status.{body['service_code']}': 'IP',
+                                    f'status.{body["service_code"]}': 'IP',
                                     'updated_at': get_brazil_datetime()
                                 }
                             }
@@ -48,16 +53,16 @@ async def lifespan(app: FastAPI):
                         crawler = CrawlerCriminal('portalbnmp.cnj.jus.br')
                         result = crawler.search(body)
 
-                        logger.info(f'[{name_queue}] Mensagem processada com sucesso.')
+                        logger.info(f'[consumer-{channel_id}] Mensagem processada com sucesso.')
 
                         body.update({'result': result})
 
                         await save_result(body['query_id'], body.get('result'), body)
 
-                        return logger.info(f'Mensagem publicada processada com sucesso {body["query_id"]}')
+                        logger.info(f'Mensagem publicada processada com sucesso {body["query_id"]}')
 
                     except Exception as e:
-                        logger.error(f'[{name_queue}] Erro ao processar mensagem: {e}')
+                        logger.error(f'[consumer-{channel_id}] Erro ao processar mensagem: {e}')
 
                         retry_count = body.get('retry_count', 0)
 
@@ -69,10 +74,10 @@ async def lifespan(app: FastAPI):
 
                             await channel.default_exchange.publish(
                                 Message(new_body.encode()),
-                                routing_key=f'publish-warrants-{settings.ENVIRONMENT}'
+                                routing_key=queue_name
                             )
 
-                            return logger.warning(f"[{name_queue}] Reenviando mensagem. Tentativa {retry_count + 1}")
+                            logger.warning(f"[consumer-{channel_id}] Reenviando mensagem. Tentativa {retry_count + 1}")
                         else:
                             body['error'] = str(e)
                             dlq_body = json.dumps(body)
@@ -86,22 +91,20 @@ async def lifespan(app: FastAPI):
                                 {'_id': body['query_id']},
                                 {
                                     '$set': {
-                                        f'status.{body['service_code']}': 'ER',
+                                        f'status.{body["service_code"]}': 'ER',
                                         'updated_at': get_brazil_datetime()
                                     }
                                 }
                             )
 
-                            return logger.error(f"[{name_queue}] Mensagem enviada para DLQ após {max_retries} tentativas.")
+                            logger.error(f"[consumer-{channel_id}] Mensagem enviada para DLQ após {max_retries} tentativas.")
 
-    tasks = [
-        asyncio.create_task(consume(queue, f'publish-warrants-{i+1}'))
-        for i in range(3)
-    ]
+    for i in range(3):
+        consumers.append(asyncio.create_task(consume(i + 1)))
 
     yield
 
-    for task in tasks:
+    for task in consumers:
         task.cancel()
 
     await connection.close()
@@ -113,6 +116,7 @@ app = FastAPI(lifespan=lifespan)
 if __name__ == '__main__':
     uvicorn.run(
         'main:app',
-        host='0.0.0.0', port=int(settings.PORT),
+        host='0.0.0.0',
+        port=int(settings.PORT),
         reload=True
     )
